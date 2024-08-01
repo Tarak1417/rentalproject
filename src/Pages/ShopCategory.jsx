cf4e59d84b361386ddd48183f564f2a4053d3246
Tarak1417 <achantatarak@gmail.com>
2024-08-01 12:16:49 +0530

    add shoeting

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,36 +1,70 @@
-import React, {useContext} from "react"
+import React, { useContext, useState } from "react";
+import "./Css/ShopCategory.css";
+import { Shopcontext } from "../Context/Shopcontext";
 
-import "./Css/ShopCategory.css"
-import { Shopcontext } from "../Context/Shopcontext"
-import dropdown_icon from "../components/Assets/dropdown_icon.png"
-import Items from "../components/Items/Items"
-const ShopCategory=(props)=>{
-    const {all_product}=useContext(Shopcontext)
+import Items from "../components/Items/Items";
+
+const ShopCategory = (props) => {
+    const { all_product } = useContext(Shopcontext);
+    const [sortOption, setSortOption] = useState("default");
+
+    // Sorting function
+    const sortProducts = (products, option) => {
+        switch (option) {
+            case "price-asc":
+                return products.slice().sort((a, b) => a.new_price - b.new_price);
+            case "price-desc":
+                return products.slice().sort((a, b) => b.new_price - a.new_price);
+            case "title-asc":
+                return products.slice().sort((a, b) => a.name.localeCompare(b.name));
+            case "title-desc":
+                return products.slice().sort((a, b) => b.name.localeCompare(a.name));
+            default:
+                return products;
+        }
+    };
+
+    // Filter and sort products
+    const filteredProducts = all_product.filter(item => props.category === item.category);
+    const sortedProducts = sortProducts(filteredProducts, sortOption);
+
     return (
         <div className="shop-category">
-           <img  className=" ShopCategory-banner" src={props.banner} alt=""/>
-          <div className="shop-catagory-index">
-            <p>
-                <span>Showing 1-12</span> out of 36 products
-            </p>
-            <div className="shopcategory-sort">
-                Sort by <img src={dropdown_icon} alt=""/>
+            <img className="ShopCategory-banner" src={props.banner} alt="" />
+            <div className="shop-catagory-index">
+                <p>
+                    <span>Showing 1-{sortedProducts.length}</span> out of {filteredProducts.length} products
+                </p>
+                
+                    <div className="dropdown">
+                    <select
+                        value={sortOption}
+                        onChange={(e) => setSortOption(e.target.value)}
+                    >
+                        <option value="default">Default</option>
+                        <option value="price-asc">Price (Low to High)</option>
+                        <option value="price-desc">Price (High to Low)</option>
+                        <option value="title-asc">Title (A to Z)</option>
+                        <option value="title-desc">Title (Z to A)</option>
+                    </select>
+
+                    </div>
             </div>
-          </div>
-          <div className="shopcategory-products">
-            {all_product.map((item,i)=>{
-                if (props.category===item.category){
-                   return <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-                }
-                else {
-                    return null;
-                }
-            })}
-          </div>
-          <div className="loadmore">
-            EXPLORE MORE
-          </div>
+            <div className="shopcategory-products">
+                {sortedProducts.map((item, i) => (
+                    <Items
+                        key={i}
+                        id={item.id}
+                        name={item.name}
+                        image={item.image}
+                        new_price={item.new_price}
+                        old_price={item.old_price}
+                    />
+                ))}
+            </div>
+            <div className="loadmore">EXPLORE MORE</div>
         </div>
-    )
-}
-export default ShopCategory
+    );
+};
+
+export default ShopCategory;
